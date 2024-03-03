5b806f53b441f30ee0bc47a3c54bfbed800f163a
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-03-02 23:17:41 -0300

    Update index.js

--- src/componentes/footer/index.js
+++ src/componentes/footer/index.js
@@ -2,7 +2,37 @@ import './footer.css'
 
 const Footer = () => {
     return (
-        
+        <footer className='footer'>
+                <section >
+            <ul>
+                <li>
+                    <a href='facebook.com' target='_blank'>
+                        <img className='img' src='\imagens\fb.png' alt='Logo do facebook '/> 
+                    </a>
+                </li>
+               <li>
+                    <a href='twitter.com' target='_blank'>
+                        <img className='img' src='\imagens\tw.png' alt='Logo do twitter'/>
+                    </a>
+                </li>
+                <li>
+                    <a href='instagram.com' target='_blank'>
+                       <img className='img' src='\imagens\ig.png' alt='Logo do instagram'/> 
+                    </a>
+                </li>
+                
+            </ul>
+        </section>
+        <section>
+            <img src='\imagens\logo.png' alt='Logo do organo' />
+        </section>
+        <section>
+            <p>
+                Desenvolvido por Edilson
+            </p>
+        </section>
+        </footer>
+    
     )
 
 }
