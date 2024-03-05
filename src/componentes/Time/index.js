948cb667584d1748f3933542b69a4dc7cf063c94
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-03-05 02:56:04 -0300

    update

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -11,7 +11,10 @@ const Time = (props) => {
         (props.collaborators.length > 0) ? <section className="time" style={color}>
             <h3 style={border}>{props.nome}</h3>
             <div className="collaborators">
-            {props.collaborators.map(Collaborator => < CardCollaborator key={Collaborator.nome} nome={Collaborator.nome} cargo={Collaborator.cargo} imagem={Collaborator.imagem}/>)}
+        {props.collaborators.map(Collaborator => {
+            return < CardCollaborator key={Collaborator.nome} nome={Collaborator.nome} cargo={Collaborator.cargo} imagem={Collaborator.imagem} toDelted={toDelted}/>
+            
+        })}
             </div>
         </section>
         : ''
